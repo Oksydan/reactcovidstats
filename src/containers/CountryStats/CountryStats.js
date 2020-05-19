ea969f61e86dea7e5129acca16de54e18f8953ac
Igor Stępień <oksydan56@gmail.com>
2020-05-20 00:03:16 +0200

    Added date picker for country stats

--- src/containers/CountryStats/CountryStats.js
+++ src/containers/CountryStats/CountryStats.js
@@ -1,34 +1,69 @@
 import React, { useState, useEffect, Fragment } from "react";
 import { useRouteMatch } from "react-router-dom";
-import { prepareData, getPrecentageValue } from "../../utils/utils";
+import { prepareData, getPrecentageValue, formatDate } from "../../utils/utils";
 import { useHistory } from "react-router-dom";
 import useFetchData from '../../hooks/useFetchData';
 import Loader from '../../components/UI/Loader/Loader';
 import DataCard from '../../components/DataCard/DataCard';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import { DatePicker, MuiPickersUtilsProvider  } from "@material-ui/pickers";
+import MomentUtils from '@date-io/moment';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  spacingBottom: {
+    marginBottom: theme.spacing(2)
+  }
+}))
+
 
 const CountryStats = props => {
   const { params } = useRouteMatch();
   const history = useHistory();
+  const classes = useStyles();
 
   const [countryStats, setCountryStats] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [selectedDate, handleDateChange] = useState(new Date());
   const fetchStatsData = useFetchData();
 
 
     
   useEffect(() => {
     let fetching = true;
-    console.log('mount')
+
+    let fetch;
+    const today = formatDate(new Date());
+    let pickedDate;
+
+    if (selectedDate instanceof Date) {
+      pickedDate = formatDate(selectedDate);
+    } else {
+      pickedDate = formatDate(selectedDate._d);
+    }
+
+    if (today !== pickedDate) {
+      fetch = fetchStatsData({
+        endPoint: 'history',
+        params: {
+          country: params.name,
+          day: pickedDate
+        }
+      })
+    } else {
+      fetch = fetchStatsData({
+        endPoint: 'statistics',
+        params: {
+          country: params.name
+        }
+      })
+    }
 
     setLoading(true);
-    fetchStatsData({
-      endPoint: 'statistics',
-      params: {
-        country: params.name
-      }
-    })
+
+    fetch
     .then((response) => response.json())
     .then((data) => {
       const { response } = data;
@@ -52,7 +87,7 @@ const CountryStats = props => {
     });
 
     return () => fetching = false;
-  }, [params.name, history, fetchStatsData]);
+  }, [params.name, history, fetchStatsData, selectedDate, formatDate]);
 
   let content = null;
 
@@ -66,6 +101,19 @@ const CountryStats = props => {
       <Typography gutterBottom variant="h3" component="h1" align="center" weight="700">
         {countryStats.name}
       </Typography>
+      
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+        <Box component="div" align="center" className={classes.spacingBottom} >
+          <DatePicker
+            autoOk
+            label="Select date"
+            disableFuture
+            value={selectedDate}
+            onChange={handleDateChange}
+          />
+        </Box>
+      </MuiPickersUtilsProvider>
+
       <Grid
         container
         spacing={3}
