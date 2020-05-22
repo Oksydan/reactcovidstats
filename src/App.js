cb4e03088a4f01e165fed17e1607db9b6a3bd3ad
Igor Stępień <oksydan56@gmail.com>
2020-05-22 22:26:32 +0200

    App general spacing

--- src/App.js
+++ src/App.js
@@ -3,23 +3,35 @@ import AllStats from './containers/AllStats/AllStats';
 import CountryStats from './containers/CountryStats/CountryStats';
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  spacing: {
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2)
+  }
+}));
 
 
-const app = () => (
-  <BrowserRouter>
-    <Container fixed>
-      <Switch>
-        <Route path="/country/:name/:date?">
-          <CountryStats />
-        </Route>
-        <Route path="/">
-          <AllStats />
-        </Route>
-      </Switch>
-    </Container>
-  </BrowserRouter>
-);
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <Container fixed className={classes.spacing}>
+        <Switch>
+          <Route path="/country/:name/:date?">
+            <CountryStats />
+          </Route>
+          <Route path="/">
+            <AllStats />
+          </Route>
+        </Switch>
+      </Container>
+    </BrowserRouter>
+  );
+};
 
 
 
-export default app;
+export default App;
